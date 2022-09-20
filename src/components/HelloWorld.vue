90afc969c3f5ae2459ab973a8d8af32847850e42
Ibwedagama <wardana.wedagama@gmail.com>
2022-09-20 21:32:00 +0800

    feat: test contextual env

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,7 +4,9 @@ defineProps({
     type: String,
     required: true,
   },
-});
+})
+
+const appEnvironment = import.meta.env.VITE_APP_ENVIRONMENT
 </script>
 
 <template>
@@ -14,16 +16,9 @@ defineProps({
     </h1>
     <h3>
       You’ve successfully created a project with
-      <a
-        href="https://vitejs.dev/"
-        target="_blank"
-        rel="noopener"
-      >Vite</a> +
-      <a
-        href="https://vuejs.org/"
-        target="_blank"
-        rel="noopener"
-      >Vue 3</a>.
+      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
+      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
+      on {{ appEnvironment }}
     </h3>
   </div>
 </template>
@@ -45,6 +40,7 @@ h3 {
 }
 
 @media (min-width: 1024px) {
+
   .greetings h1,
   .greetings h3 {
     text-align: left;
